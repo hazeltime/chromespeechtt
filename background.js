be8b3c9793128d541a1a2e9101e0d011a385e792
Behrouz Talebi <berrat2@gmail.com>
2024-08-15 15:06:57 +0200

    Toggle

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.commands.onCommand.addListener((command) => {
     if (command === "start-speech-to-text") {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { command: "start-speech-to-text" }, function (response) {
+            chrome.tabs.sendMessage(tabs[0].id, { command: "toggle-speech-to-text" }, function (response) {
                 if (chrome.runtime.lastError) {
                     console.error("Could not send message: ", chrome.runtime.lastError.message);
                 } else {
-                    console.log("Message sent successfully");
+                    console.log("Speech recognition toggled:", response.status);
                 }
             });
         });
